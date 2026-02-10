419a984e610f3edcc06ae7faf2ddd8becf19acdc
Kevin.Franco <keivnfranco1623@gmail.com>
2026-02-09 23:05:49 -0500

    panel administracion y primera vista del producto desde supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { supabase } from "@/lib/supabase";
+import Image from "next/image";
 
 export default async function Home() {
   const { data: products, error } = await supabase
@@ -21,6 +22,25 @@ export default async function Home() {
         </h2>
 
         {error && (<p className="text-red-500 mb-4">Error: {error.message}</p>)}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products?.map((product) => (
+            <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+              <div>
+                <Image src={product.image_url} alt={product.name} width={300} height={300} className="w-full h-48 object-cover" />
+              </div>
+
+              <div className="p-4">
+                <h3 className="text-lg font-bold text-gray-800 mb-2">{product.name}</h3>
+                <p className="text-gray-600 mb-4">{product.description}</p>
+                
+                <div>
+                  <span className="text-gray-600 mb-4">{product.price.toLocaleString('es-CO')}</span>
+                  <span className="text-gray-600 mb-4">{product.category}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
 
         {products && products.length === 0 && (
           <p className="text-gray-600 mb-4">No hay productos aún. Agrega algunos desde el panel admin.</p>
